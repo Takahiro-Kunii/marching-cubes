0a2c9e508ada5010b5850818cb60ff060784eea6
Takahiro Kunii <pub.xcc@gmail.com>
2020-05-22 17:37:10 +0900

    gridへの値構築法を変更

--- js/main-marching-cubes.js
+++ js/main-marching-cubes.js
@@ -1,44 +1,19 @@
-//	0.0 〜 1.0 と 0 〜 size を写像する
-class Mapper {
-    constructor(size) {
-		this.size = size;
-	}
-	index(v) {
-		return Math.floor(v * this.size);
-	}
-	regular(v) {
-		return v / this.size;
-	}
-};
-
-//	格子に球体の分布を追加
-//	
-//	格子外へのはみ出しは考慮してない
-//	球体の表面を1とし、中心に向け増加する値を設定、表面より外は1より小さい
-//	grid:1次元配列
-//	size：1次元配列gridを3次元に展開するための折り返し数
-//	 	各次元の折り返し数とすることで、1次元grid[i]を3次元grid[z][y][x] とみなす
-//	x, y, z:球体の中心（1.0 = size という単位）
-//	r:球体の半径（同上）
-function addSphere(grid, size, x, y, z, r) {
-	m = new Mapper(size);
-	for (let k = m.index( z - r ); k < m.index( z + r ); k++) {
-		for (let j = m.index( y - r ); j < m.index( y + r ); j++) {
-			for (let i = m.index( x - r ); i < m.index( x + r ); i++) {
-				//	i, j, kで特定される格子接続点の値を計算
-				let dz = m.regular(k) - z;
-				let dy = m.regular(j) - y;
-				let dx = m.regular(i) - x;
-				let d = dx * dx + dy * dy + dz * dz;
-				let value = 10000;
-				if (d != 0)
-					value = r / Math.sqrt(d);
-				//	i, j, kで特定される格子接続点のインディックスを計算し値を設定
-				let index = (size * size * k) + (size * j) + i;
-				grid[ index ] += value;
+//	受け取った f(x, y, z) 関数を使い、その戻り値をgridの値に加算する
+//	fに渡すx, y, zはgridの 0 〜 size 範囲を -1 〜 1 に写像させたものとする。
+//	gridを3次元配列と考えた場合のgrid[k][j][i]のk, j, iはそれぞれ空間座標のz, y, xに対応させる。
+function addFunction(grid, size, func) {
+	for (let k = 0; k < size; k++) {
+		for (let j = 0; j < size; j++) {
+			for (let i = 0; i < size; i++) {
+				let index = (size * size * k) + (size * j) + i;				
+				//	gridの 0 〜 size 範囲を -1 〜 1 に写像
+				let x = i * 2 / size - 1;
+				let y = j * 2 / size - 1;
+				let z = k * 2 / size - 1;
+				grid[index] += func(x, y, z);
 			}
 		}
-	}
+	}	
 }
 
 window.onload = function (e) {
@@ -47,9 +22,16 @@ window.onload = function (e) {
 	lab.setup({shadow:true, useTrackball:true, floor:{width:60, height:60}});
 
 	//	マーチングキューブス法を実行するオブジェクト
-	let mc = new MarchingCubes( 20, 1.4 );
-	addSphere(mc.grid, mc.size, 0.5, 0.5, 0.5, 0.3);
+	let mc = new MarchingCubes(20, 0.0);
+	//	gridに濃度を設定
+	addFunction(mc.grid, mc.size, (x, y, z) => {
+		//	y = x^2 の式
+		//	return (x * x) - y;
 
+		//	半径0.5 原点を中心にした球体の式　
+		return 0.5 - Math.sqrt(x * x + y * y + z * z);
+	});
+	
 	//	マーチングキューブス法で作ったポリゴンを表示するオブジェクト
 	let material = new THREE.MeshPhongMaterial( { color: 0x00ff00, specular: 0x111111, shininess: 1 } );
 	let o = new THREE.ImmediateRenderObject(material);
